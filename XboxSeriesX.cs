45491045e1cfe67a0b0d9a52fd8efd63dd7d5161
StatistikChris <keinepanikvorstatistik@gmail.com>
2026-03-10 14:26:54 +0100

    xbox class created

---
+++ XboxSeriesX.cs
@@ -0,0 +1,13 @@
+public class XboxSeriesX
+{
+    public string Manufacturer { get; set; } = "Microsoft";
+    public string Name { get; set; } = "Xbox Series X";
+    public int ReleaseYear { get; set; } = 2020;
+
+    public void ShowInfo()
+    {
+        Console.WriteLine($"Manufacturer: {Manufacturer}");
+        Console.WriteLine($"Name: {Name}");
+        Console.WriteLine($"Release Year: {ReleaseYear}");
+    }
+}
