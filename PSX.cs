54aae3e41154dc15f6f087c112212a68f2c57d04
richardkab <richard.kabanov@gmail.com>
2026-03-10 14:21:46 +0100

    Psx update

---
+++ PSX.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace RetroConsole
+{
+       public class Psx
+    {
+    
+        public string Name;
+        public int ReleaseYear;
+        public int BitCount;
+        public string Manufacturer;
+
+        public Psx()
+        {
+            Name = "PlayStation 1";
+            ReleaseYear = 1994;
+            BitCount = 32;
+            Manufacturer = "Sony";
+        } 
+    }
+}      
--- Program.cs
+++ Program.cs
@@ -1,3 +1,18 @@
-﻿
+﻿using System;
 
-Console.WriteLine("Hello, World!");
+namespace RetroConsole 
+{
+class Program
+    {
+        static void Main(string[] args)
+        {
+            Psx myConsole = new Psx();
+
+            Console.WriteLine("--- Infos zur Konsole ---");
+            Console.WriteLine("Modell: " + myConsole.Name);
+            Console.WriteLine("Jahr: " + myConsole.ReleaseYear);
+            Console.WriteLine("Technik: " + myConsole.BitCount + "-Bit");
+            Console.WriteLine("Hersteller: " + myConsole.Manufacturer);
+        }
+    }
+}
