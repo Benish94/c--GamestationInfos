97c9e357e9482ebe7dc84918c0233dd51894a338
benish <seidel.benjamin94@gmail.com>
2026-03-11 16:02:06 +0100

    JSON-File Generated

--- Program.cs
+++ Program.cs
@@ -1,15 +1,26 @@
 ﻿using System.Reflection;
+using System.Text.Json;
 using GamestationInfo;
 
-List<IConsole> consoles = Assembly.GetExecutingAssembly()
+List<Konsole> consoles = Assembly.GetExecutingAssembly()
     .GetTypes()
     .Where(t => typeof(Konsole).IsAssignableFrom(t) && !t.IsAbstract)
-    .Select(t => (IConsole)Activator.CreateInstance(t)!)
+    .Select(t => (Konsole)Activator.CreateInstance(t)!)
     .ToList();
 
-foreach (IConsole console in consoles)
+foreach (Konsole console in consoles)
 {
     console.ShowInfo();
     console.ShowStock();
     Console.WriteLine();
 }
+
+var options = new JsonSerializerOptions
+{
+    WriteIndented = true
+};
+
+string json = JsonSerializer.Serialize(consoles, options);
+File.WriteAllText("konsolen.json", json);
+
+Console.WriteLine("JSON Datei wurde erstellt.");
