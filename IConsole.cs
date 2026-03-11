cb51f530572d94d495284e64388617d2d88d60e4
MikaH <keule7682@gmail.com>
2026-03-11 12:03:33 +0100

    Michael Test

--- IConsole.cs
+++ IConsole.cs
@@ -1,4 +1,9 @@
 public interface IConsole
 {
+    string Name { get; set; }
+    int ReleaseYear { get; set; }
+    int Bit { get; set; }
+    string Manufacturer { get; set; }
     void ShowInfo();
+    void ShowStock();
 }
